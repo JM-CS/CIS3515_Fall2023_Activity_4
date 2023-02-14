85dec2920b48b116106133a9d1533d9577d1f8e4
Jason Michel <tuk94727@temple.edu>
2023-02-14 15:17:40 -0500

    Added Recycler Adapter to display text sizes from array in linear layout

--- app/src/main/java/edu/temple/activity4/MainActivity.kt
+++ app/src/main/java/edu/temple/activity4/MainActivity.kt
@@ -2,8 +2,14 @@ package edu.temple.activity4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,11 +23,39 @@ class MainActivity : AppCompatActivity() {
         // Trying to create array of integers that are multiples of 5
         // Verify correctness by examining array values.
         val textSizes = Array(20){(it + 1) * 5}
+        //Log.d("Array vals", textSizes.contentToString())
+        textSizeSelector = findViewById(R.id.textSizeSelectorRecyclerView)
+        textSizeDisplay= findViewById(R.id.textSizeDisplayTextView)
+
+        textSizeSelector.adapter = TextSizeAdapter(textSizes)
+        textSizeSelector.layoutManager = LinearLayoutManager(this)
     }
 }
 
 
 /* Convert to RecyclerView.Adapter */
-class TextSizeAdapter {
+class TextSizeAdapter(_textSizes: Array<Int>) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
 
+    val textSizes = _textSizes
+
+    class TextSizeViewHolder(view: TextView) : RecyclerView.ViewHolder(view){
+        val textView = view
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextSizeViewHolder {
+        return TextSizeViewHolder(TextView(parent.context).apply{
+            setPadding(5,20,0,20)
+        })
+    }
+
+    override fun onBindViewHolder(holder: TextSizeViewHolder, position: Int) {
+        holder.textView.apply{
+            text = textSizes[position].toString()
+            textSize = textSizes[position].toFloat()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return textSizes.size
+    }
 }
